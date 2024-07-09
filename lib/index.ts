90e1829779af2fd0369176ab60ca5dd0cdbec7dc
Branko <saicoder.net@gmail.com>
2024-07-10 00:07:54 +0200

    Export Endian enum

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export { TIFFImage } from './tiff-image'
+export { TIFFImage, Endian } from './tiff-image'
 export { TagType, type TagTypeKeys } from './tag-types'
 export { TagName } from './tag-names'
 export { initRuntime } from './runtime'
